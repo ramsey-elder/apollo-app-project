81cf49e346cae507a8f8e7a8035e3cc5e3dc1305
Nicholas Lee <nicholasl3688@gmail.com>
2026-04-15 16:36:36 -0400

    Added club building user data and renamed main

---
+++ database-files/02_buildings_data.sql
@@ -0,0 +1,21 @@
+INSERT INTO buildings (building_id, building_name, street, city, state, zip, creator_id) VALUES
+(1, 'Snell Library', '360 Huntington Ave', 'Boston', 'MA', '02115', 1),
+(2, 'Curry Student Center', '346 Huntington Ave', 'Boston', 'MA', '02115', 2),
+(3, 'Marino Recreation Center', '259-267 Huntington Ave', 'Boston', 'MA', '02115', 1),
+(4, 'SquashBusters Center', '247 Huntington Ave', 'Boston', 'MA', '02115', 2),
+(5, 'ISEC', '805 Columbus Ave', 'Boston', 'MA', '02120', 1),
+(6, 'Dodge Hall', '360 Huntington Ave', 'Boston', 'MA', '02115', 2),
+(7, 'Richards Hall', '360 Huntington Ave', 'Boston', 'MA', '02115', 1),
+(8, 'Behrakis Health Sciences Center', '30 Leon St', 'Boston', 'MA', '02115', 2),
+(9, 'Shillman Hall', '115 Forsyth St', 'Boston', 'MA', '02115', 1),
+(10, 'Hayden Hall', '370 Huntington Ave', 'Boston', 'MA', '02115', 2),
+(11, 'Ryder Hall', '11 Leon St', 'Boston', 'MA', '02115', 1),
+(12, 'Ell Hall', '346 Huntington Ave', 'Boston', 'MA', '02115', 2),
+(13, 'Churchill Hall', '380 Huntington Ave', 'Boston', 'MA', '02115', 1),
+(14, 'West Village H', '440 Huntington Ave', 'Boston', 'MA', '02115', 2),
+(15, 'EXP', '805 Columbus Ave', 'Boston', 'MA', '02120', 1),
+(16, 'Carter Field', '400 Huntington Ave', 'Boston', 'MA', '02115', 2),
+(17, 'Matthews Arena', '238 St Botolph St', 'Boston', 'MA', '02115', 1),
+(18, 'Lake Hall', '512 Huntington Ave', 'Boston', 'MA', '02115', 2),
+(19, 'Nightingale Hall', '105 Forsyth St', 'Boston', 'MA', '02115', 1)
+;
