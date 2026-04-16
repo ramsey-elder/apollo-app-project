6d353f3222579443c57fec4bd68c5317405c7e2c
ramsey-elder <elder.r@northeastern.edu>
2026-04-15 20:01:18 -0400

    added features data sql

---
+++ database-files/12_features_data.sql
@@ -0,0 +1,6 @@
+INSERT INTO features (event_id, nupd, catering, photographer, folding_table, emt, sound_system) VALUES
+(1, FALSE, FALSE, TRUE,  TRUE,  FALSE, FALSE),
+(2, FALSE, FALSE, TRUE,  FALSE, FALSE, TRUE),
+(3, FALSE, TRUE,  TRUE,  TRUE,  FALSE, FALSE),
+(4, FALSE, FALSE, FALSE, FALSE, FALSE, TRUE),
+(5, TRUE,  TRUE,  FALSE, TRUE,  TRUE,  FALSE);
